49a39ac35359fb7cae27f558b5daa441dbdf5974
Adam Miller <miad0801@gmail.com>
2019-10-13 07:49:48 -0500

    Added burgers

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Breakfast burger");
+INSERT INTO burgers (burger_name) VALUES ("Quarter pounder");
+INSERT INTO burgers (burger_name) VALUES ("Deluxe burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
